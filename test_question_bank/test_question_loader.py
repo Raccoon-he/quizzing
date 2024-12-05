7b564925628a52f1c1f4fd7dc1bb34e1551d59ab
HuanHe <huanhe@student.ubc.ca>
2024-12-04 16:17:42 -0800

    add test_subpackage1

---
+++ test_question_bank/test_question_loader.py
@@ -0,0 +1,40 @@
+import unittest
+from quizzing.question_bank.question_loader import questionloader
+from quizzing.question_bank.question_manager import questionmanager
+
+
+class TestQuestionLoader(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print("Set up the test class.")
+
+    def setUp(self):
+        print("Set up the question bank.")
+        self.ql1 = questionloader(questionmanager())
+        self.ql2 = questionloader(questionmanager())
+        self.questions1 = self.ql1.load_questions_from_file("test1.csv")
+        self.questions2 = self.ql2.load_questions_from_file("test2.json")
+
+    def tearDown(self):
+        print("Tear down the question bank.")
+        self.questions1 = None
+        self.questions2 = None
+
+    @classmethod
+    def tearDownClass(cls):
+        print("Tear down the test class.")
+
+    def test_category(self):
+        self.assertEqual(self.ql1.classify_questions_by_category(self.questions1)['geography'], 5)
+        self.assertEqual(self.ql1.classify_questions_by_category(self.questions1)['science'], 5)
+        self.assertEqual(self.ql2.classify_questions_by_category(self.questions2)['history'], 4)
+        self.assertEqual(self.ql2.classify_questions_by_category(self.questions2)['languages'], [])
+
+    def test_difficulty(self):
+        self.assertEqual(self.ql1.classify_questions_by_difficulty(self.questions1)['easy'], 10)
+        self.assertEqual(self.ql1.classify_questions_by_difficulty(self.questions1)['medium'], 14)
+        self.assertEqual(self.ql1.classify_questions_by_difficulty(self.questions1)['hard'], 1)
+        self.assertEqual(self.ql2.classify_questions_by_difficulty(self.questions2)['easy'], 10)
+        self.assertEqual(self.ql2.classify_questions_by_difficulty(self.questions2)['medium'], 12)
+        self.assertEqual(self.ql2.classify_questions_by_difficulty(self.questions2)['hard'], 2)
+
---
+++ test_question_bank/test_question_manager.py
@@ -0,0 +1,80 @@
+import unittest
+from quizzing.question_bank.question_manager import questionmanager as qm
+
+class TestQuestionManager(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print("Set up the test class.")
+
+    def setUp(self):
+        print("Set up the question manager.")
+        self.manager = qm()
+        self.questions = [
+            {
+                "id": "20",
+                "text": "When did the Roman Empire fall? A) 376 AD B) 476 AD C) 576 AD D) 676 AD",
+                "answer": "B",
+                "difficulty": "medium",
+                "category": "history"
+            },
+            {
+                "id": "21",
+                "text": "What is the boiling point of water? A) 90\u00a1C B) 95\u00a1C C) 100\u00a1C D) 105\u00a1C",
+                "answer": "C",
+                "difficulty": "easy",
+                "category": "science"
+            },
+            {
+                "id": "22",
+                "text": "What is the capital of Russia? A) St. Petersburg B) Moscow C) Kiev D) Minsk",
+                "answer": "B",
+                "difficulty": "easy",
+                "category": "geography"
+            },
+            {
+                "id": "23",
+                "text": "What is 3^2 + 4^2? A) 20 B) 25 C) 30 D) 35",
+                "answer": "B",
+                "difficulty": "medium",
+                "category": "mathematics"
+            },
+            {
+                "id": "24",
+                "text": "Which company owns Android? A) Apple B) Microsoft C) Google D) Samsung",
+                "answer": "C",
+                "difficulty": "easy",
+                "category": "technology"
+            }
+        ]
+
+    def tearDown(self):
+        print("Tear down the question manager.")
+        self.manager = None
+        self.questions = None
+
+    @classmethod
+    def tearDownClass(cls):
+        print("Tear down the test class.")
+
+    def test_add_question(self):
+        self.assertEqual(len(qm.question_bank), 0)
+        self.manager.add_question(self.questions[0])
+        self.assertEqual(len(qm.question_bank), 1)
+        self.assertEqual(qm.question_bank[0]['id'], 20)
+        self.manager.add_question(self.questions[1])
+        self.manager.add_question(self.questions[2])
+        self.manager.add_question(self.questions[3])
+        self.manager.add_question(self.questions[4])
+        self.assertEqual(len(qm.question_bank), 5)
+        self.assertEqual(qm.question_bank[4]['id'], 24)
+
+    def test_remove_question(self):
+        self.manager.remove_question(20)
+        self.assertEqual(len(qm.question_bank), 4)
+        self.assertEqual(qm.question_bank[3]['id'], 24)
+        self.manager.remove_question(24)
+        self.assertEqual(len(qm.question_bank), 3)
+        self.assertEqual(qm.question_bank[2]['id'], 23)
+
+
+
